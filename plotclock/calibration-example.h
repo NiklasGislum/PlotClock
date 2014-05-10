0e8bcc38a292b60386865ef07491275f721603e1
Flemming Frandsen <dren.dk@gmail.com>
2014-05-10 14:03:56 +0200

    Added ignore file for the calibration file

---
+++ plotclock/calibration-example.h
@@ -0,0 +1,15 @@
+//#define CALIBRATION      // enable calibration mode
+
+#define SERVOFAKTORLEFT 590
+#define SERVOFAKTORRIGHT 600
+
+// Zero-position of left and right servo
+// When in calibration mode, adjust the NULL-values so that the servo arms are at all times parallel
+// either to the X or Y axis
+#define SERVOLEFTNULL 2320
+#define SERVORIGHTNULL 1010
+
+
+#define LIFT0 1150 // on drawing surface
+#define LIFT1 825  // between numbers
+#define LIFT2 725  // going towards sweeper
